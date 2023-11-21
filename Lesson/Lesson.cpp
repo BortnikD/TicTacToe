dfa869e8fa34d15eb3a2dbb09a5ed05650834447
BortnikD <147266972+BortnikD@users.noreply.github.com>
2023-11-21 19:16:35 +0300

    TicTacToe 2.0
    1) Зациклил всю программу для возможности повторения игры без перезапуска.
    2) Теперь можно играть с ботом на легком уровне сложности (пока что единственном)
    3) Местами улучшил читабельность кода

--- Lesson/Lesson.cpp
+++ Lesson/Lesson.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <ctime>
 using namespace std;
 
 const int ROWS = 3, COLS = 3;
@@ -18,7 +19,7 @@ int chooseCell ()
 	cout << "Выберите ячейку!\nВаш выбор: ";
 	int cell;
 	cin >> cell;
-	cout << endl;
+	/*cout << endl;*/
 	int row = (cell - 1) / COLS;
 	int col = (cell - 1) % COLS;
 	if (cin.fail())
@@ -54,6 +55,7 @@ void arrOut()
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
 
 int checkWinX() {
@@ -101,6 +103,7 @@ int checkWin0()
 		return 1;
 	}
 }
+
 bool checkDraw()
 {
 	for (int i = 0; i < ROWS; i++)
@@ -116,63 +119,54 @@ bool checkDraw()
 	return true;
 }
 
-int main()
+int playWithFriend()
 {
-	setlocale(LC_ALL, "ru");
-	cout << "Здравствуйте!\n\n";
-
-	for (int i = 0; i < ROWS; ++i) {
-		for (int j = 0; j < COLS; ++j) {
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
-	}
-	while (true) 
+	arrOut();
+	while (true)
 	{
-		
 		cout << "Ход игрока Х!\n";
-	
-			switch (chooseCell())
-			{
-			case 1:
-				arr[0][0] = "[x]";
-				arrOut();
-				break;
-			case 2:
-				arr[0][1] = "[x]";
-				arrOut();
-				break;
-			case 3:
-				arr[0][2] = "[x]";
-				arrOut();
-				break;
-			case 4:
-				arr[1][0] = "[x]";
-				arrOut();
-				break;
-			case 5:
-				arr[1][1] = "[x]";
-				arrOut();
-				break;
-			case 6:
-				arr[1][2] = "[x]";
-				arrOut();
-				break;
-			case 7:
-				arr[2][0] = "[x]";
-				arrOut();
-				break;
-			case 8:
-				arr[2][1] = "[x]";
-				arrOut();
-				break;
-			case 9:
-				arr[2][2] = "[x]";
-				arrOut();
-				break;
-			default:
-				cout << "Данной ячейки не существует";
-			}
+
+		switch (chooseCell())
+		{
+		case 1:
+			arr[0][0] = "[x]";
+			arrOut();
+			break;
+		case 2:
+			arr[0][1] = "[x]";
+			arrOut();
+			break;
+		case 3:
+			arr[0][2] = "[x]";
+			arrOut();
+			break;
+		case 4:
+			arr[1][0] = "[x]";
+			arrOut();
+			break;
+		case 5:
+			arr[1][1] = "[x]";
+			arrOut();
+			break;
+		case 6:
+			arr[1][2] = "[x]";
+			arrOut();
+			break;
+		case 7:
+			arr[2][0] = "[x]";
+			arrOut();
+			break;
+		case 8:
+			arr[2][1] = "[x]";
+			arrOut();
+			break;
+		case 9:
+			arr[2][2] = "[x]";
+			arrOut();
+			break;
+		default:
+			cout << "Данной ячейки не существует";
+		}
 
 
 		if (checkWinX())
@@ -241,6 +235,212 @@ int main()
 		{
 			cout << "Ничья!\n";
 			return 0;
+		}
+	}
+}
+
+int botChooseEasy()
+{
+	int cell = rand() % 9 + 1;
+	int row = (cell - 1) / COLS;
+	int col = (cell - 1) % COLS;
+
+		if (cellOccupied[row][col]) {
+			return botChooseEasy();
+		}
+		else
+		{
+			cellOccupied[row][col] = true;
+			cout << "Выбор бота: " << cell<<endl;
+			return cell;
+		}
+}
+
+int playWithBotEasy()
+{
+	arrOut();
+	while (true)
+	{
+
+		cout << "Ваш ход!\n";
+
+		switch (chooseCell())
+		{
+		case 1:
+			arr[0][0] = "[x]";
+			arrOut();
+			break;
+		case 2:
+			arr[0][1] = "[x]";
+			arrOut();
+			break;
+		case 3:
+			arr[0][2] = "[x]";
+			arrOut();
+			break;
+		case 4:
+			arr[1][0] = "[x]";
+			arrOut();
+			break;
+		case 5:
+			arr[1][1] = "[x]";
+			arrOut();
+			break;
+		case 6:
+			arr[1][2] = "[x]";
+			arrOut();
+			break;
+		case 7:
+			arr[2][0] = "[x]";
+			arrOut();
+			break;
+		case 8:
+			arr[2][1] = "[x]";
+			arrOut();
+			break;
+		case 9:
+			arr[2][2] = "[x]";
+			arrOut();
+			break;
+		default:
+			cout << "Данной ячейки не существует";
+		}
+
+
+		if (checkWinX())
+		{
+			cout << "Вы победили!\n";
+			return 0;
+		}
+
+		if (checkWin0())
+		{
+			cout << "Бот победил!\n";
+			return 0;
+		}
+
+		if (checkDraw())
+		{
+			cout << "Ничья!\n";
+			return 0;
+		}
+
+		cout << "Ход бота!\n";
+
+		switch (botChooseEasy())
+		{
+		case 1:
+			arr[0][0] = "[0]";
+			arrOut();
+			break;
+		case 2:
+			arr[0][1] = "[0]";
+			arrOut();
+			break;
+		case 3:
+			arr[0][2] = "[0]";
+			arrOut();
+			break;
+		case 4:
+			arr[1][0] = "[0]";
+			arrOut();
+			break;
+		case 5:
+			arr[1][1] = "[0]";
+			arrOut();
+			break;
+		case 6:
+			arr[1][2] = "[0]";
+			arrOut();
+			break;
+		case 7:
+			arr[2][0] = "[0]";
+			arrOut();
+			break;
+		case 8:
+			arr[2][1] = "[0]";
+			arrOut();
+			break;
+		case 9:
+			arr[2][2] = "[0]";
+			arrOut();
+			break;
+		default:
+			cout << "Данной ячейки не существует";
+		}
+
+		if (checkWinX())
+		{
+			cout << "Вы победили!\n";
+			return 0;
+		}
+
+		if (checkWin0())
+		{
+			cout << "Вы проиграли =(\n";
+			return 0;
+		}
+
+		if (checkDraw())
+		{
+			cout << "Ничья!\n";
+			return 0;
+		}
+	}
+}
+
+int main()
+{
+	srand(time(0));
+	setlocale(LC_ALL, "ru");
+	cout << "Здравствуйте!\n";
+	while (true)
+	{
+		int progNumb;
+		cout << "Выберите как хотите сыграть: \n1) С другом \n2) C ботом \n0) Выход \nВаш выбор: ";
+		cin >> progNumb;
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Введите корректное число.\n";
+			continue;
+		}
+		cout << endl;
+		{
+			switch (progNumb)
+			{
+			case 0:
+				cout << "Спасибо за игру, до свидания!\n";
+				return 0;
+			case 1:
+				playWithFriend();
+				break;
+			case 2:
+				int diffLvl;
+				cout << "Выберите на какой сложности хотите сыграть: \n1) Легкий \nВаш выбор: ";
+				cin >> diffLvl;
+				if (cin.fail())
+				{
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					cout << "Введите корректное число.\n";
+					continue;
+				}
+				cout << endl;
+				switch (diffLvl)
+				{
+				case 1:
+					playWithBotEasy();
+					break;
+				default:
+					cout << "Данного уровня сложности не существует \n";
+				}
+
+				break;
+			default:
+				cout << "Режима с такими номером не существует \n";
+			}
 		}
 	}
 }
